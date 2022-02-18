0bede37f09425cfba5b0c6af92981684e8875fde
eduardopietre <pietre.eduardo@gmail.com>
2022-02-18 12:34:17 -0300

    Working on plugin support.

--- MoodleTelegramBot/Plugins/BasePlugin/__init__.py
+++ MoodleTelegramBot/Plugins/BasePlugin/__init__.py
@@ -1,20 +1,42 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from datetime import datetime, timezone, timedelta
+from typing import Optional
 
 
 @dataclass
 class PluginResult:
-    new: bool
     plugin_name: str
-    messages: [str]
+    new: bool
+    message: Optional[str]
+    error: Optional[Exception]
+    date: datetime
+
+    def is_empty(self):
+        return (not self.message) and (self.error is None) and (not self.new)
+
+    def strftime(self):
+        local_date = self.date.astimezone(tz=timezone(-timedelta(hours=3)))
+        return local_date.strftime("%d/%m/%Y %H:%M:%S")
+
+    def message(self):
+        if self.is_empty():
+            return f"[{self.strftime()}]\nÚltimo resultado não encontrou novas alterações."
+
+        return f"{self.plugin_name} alterações encontradas em [{self.strftime()}]:\n{self.message}"
+
+    @staticmethod
+    def empty():
+        return PluginResult("-", False, None, None, datetime.now())
 
 
 class BasePlugin(ABC):
 
-    def __init__(self, plugin_name: str, init_dictionary: dict):
-        self.plugin_name = plugin_name
+    def __init__(self, init_dictionary: dict):
+        self.plugin_name = self.__class__.__name__
         self.init_dictionary = init_dictionary
+        self.display_name = self.init_dictionary["display_name"]
 
     @abstractmethod
-    def run_scrapper(self) -> PluginResult:
+    def run_scrapper(self, callback):
         pass
