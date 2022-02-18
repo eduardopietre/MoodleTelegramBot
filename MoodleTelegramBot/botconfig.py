c3d913de5d4f7903761c88f6b5d99cb5bf947fa8
eduardopietre <pietre.eduardo@gmail.com>
2022-02-17 21:53:23 -0300

    Working on plugin support.

--- MoodleTelegramBot/botconfig.py
+++ MoodleTelegramBot/botconfig.py
@@ -14,9 +14,28 @@ class BotConfig:
     check_delay: int
     username_chat_id_cache: str
     courses: [CourseConfig]
+    plugins: dict
 
     @staticmethod
     def from_dict(d: dict, un_cypher):
+
+        def _plugin_un_cypher(orig_plugin_dict):
+            plugin_dict = {}
+
+            for k, v in orig_plugin_dict.items():
+                # Use recursion when encountering a dict
+                if type(v) == dict:
+                    v = _plugin_un_cypher(v)
+
+                # If key ends with _cypher then un_cypher it
+                if k.endswith("__cypher"):
+                    plugin_dict[k.replace("__cypher", "")] = un_cypher(v)
+                else:
+                    plugin_dict[k] = v
+
+            return plugin_dict
+
+
         return BotConfig(
             telegram_bot_token=d["telegram_bot_token"],
             notify=d["notify"],
@@ -33,6 +52,7 @@ class BotConfig:
                     name=sub_d["name"],
                     parser=sub_d["parser"]
                 ) for sub_d in d["courses"]
-            ]
+            ],
+            plugins=_plugin_un_cypher(d["plugins"])
         )
 
